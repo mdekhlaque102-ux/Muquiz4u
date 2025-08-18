dbd536f40ea5a07842cc05c59238f4d1ac3e050b
mdekhlaque102-ux <mdekhlaque102@gmail.com>
2025-08-19 04:34:37 +0530

    Update _urdu-manifest.js

--- data/urdu /_urdu-manifest.js
+++ data/urdu /_urdu-manifest.js
@@ -1,1 +1,9 @@
-
+const urduData = {
+  "subjectTitle": "urdu",
+  "chapters": {
+    "ch1": {
+      "title": "Chapter 1: Nazir Ahmad ki Kahani (Khaka)",
+      "questions": _urdu-manifest.js // Data from chapter1.js
+    },
+  }
+};
