509e65b5d90316094d0e9df9082794735fa32e5c
mdekhlaque102-ux <mdekhlaque102@gmail.com>
2025-08-19 03:31:47 +0530

    Create main-manifest.js

---
+++ data/main-manifest.js
@@ -0,0 +1,8 @@
+// File: /data/main-manifest.js
+// This is the main file that combines all subjects into one place.
+
+const quizDataBySubject = {
+  "science": scienceData,   // Data from _science-manifest.js
+  "history": historyData    // Data from _history-manifest.js
+};
+
