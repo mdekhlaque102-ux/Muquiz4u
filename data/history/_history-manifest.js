73d35b4dd431cca1cd22877e40a085951201f8c6
mdekhlaque102-ux <mdekhlaque102@gmail.com>
2025-08-19 03:29:20 +0530

    Create _history-manifest.js

---
+++ data/history/_history-manifest.js
@@ -0,0 +1,13 @@
+// File: /data/history/_history-manifest.js
+// This file combines all chapters for the History subject.
+
+const historyData = {
+  "subjectTitle": "Itihas (History)",
+  "chapters": {
+    "ch1": {
+      "title": "Chapter 1: Aadhunik Bharat",
+      "questions": history_ch1_questions // Data from chapter1.js
+    }
+  }
+};
+
