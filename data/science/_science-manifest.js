415512d9278c2519779941aeb84173aee9d187c9
mdekhlaque102-ux <mdekhlaque102@gmail.com>
2025-08-19 03:23:44 +0530

    Create _science-manifest.js

---
+++ data/science/_science-manifest.js
@@ -0,0 +1,17 @@
+// File: /data/science/_science-manifest.js
+// Yeh file Vigyan (Science) subject ke saare chapters ko jodti hai.
+
+const scienceData = {
+  "subjectTitle": "Vigyan (Science)",
+  "chapters": {
+    "ch1": {
+      "title": "Chapter 1: Mool Sidhant",
+      "questions": science_ch1_questions // Data from chapter1.js
+    },
+    "ch2": {
+      "title": "Chapter 2: Hamara Paryavaran",
+      "questions": science_ch2_questions // Data from chapter2.js
+    }
+  }
+};
+
