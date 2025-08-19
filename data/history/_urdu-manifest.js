2ddd63d6961104418b0624d05be56ba2a5b65c5a
mdekhlaque102-ux <mdekhlaque102@gmail.com>
2025-08-19 12:51:16 +0530

    Update and rename _history-manifest.js to _urdu-manifest.js

--- data/history/_urdu-manifest.js
+++ data/history/_urdu-manifest.js
@@ -1,12 +1,12 @@
 // File: /data/history/_history-manifest.js
 // This file combines all chapters for the History subject.
 
-const historyData = {
+const urduData = {
   "subjectTitle": "History",
   "chapters": {
     "ch1": {
       "title": "Chapter 1: Aadhunik Bharat",
-      "questions": history_ch1_questions // Data from chapter1.js
+      "questions": urdu_ch1_questions // Data from chapter1.js
     }
   }
 };
