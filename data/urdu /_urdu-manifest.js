ecc99b1aa31deed7ad0404a4d2a349b4c2563ec7
mdekhlaque102-ux <mdekhlaque102@gmail.com>
2025-08-19 10:24:54 +0530

    Delete data/urdu /_urdu-manifest.js

--- data/urdu /_urdu-manifest.js
+++
@@ -1,9 +0,0 @@
-const urduData = {
-  "subjectTitle": "urdu",
-  "chapters": {
-    "ch1": {
-      "title": "Chapter 1: Nazir Ahmad ki Kahani (Khaka)",
-      "questions": _urdu-manifest.js // Data from chapter1.js
-    },
-  }
-};
