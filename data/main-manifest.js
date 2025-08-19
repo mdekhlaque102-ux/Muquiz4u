8bf4615cedff15199ad30196aa880ce695fe7020
mdekhlaque102-ux <mdekhlaque102@gmail.com>
2025-08-19 10:26:05 +0530

    Update main-manifest.js

--- data/main-manifest.js
+++ data/main-manifest.js
@@ -3,7 +3,6 @@
 
 const quizDataBySubject = {
   "science": scienceData,   // Data from _science-manifest.js
-  "history": historyData,   // Data from _history-manifest.js
-  "urdu": urduData,
+  "history": historyData,   // Data from _history-manifest.j
 };
 
